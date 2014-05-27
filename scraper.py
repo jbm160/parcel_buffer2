94a7a8205766fff8da0bb1c6baa555193df5ff36
jbm160 <jbm160@hotmail.com>
2014-05-26 22:54:32 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -183,7 +183,7 @@ def getAppraisal(objectID,parcelID):
             data = card.cssselect('td')
             i = 0
             while i < len(data):
-                print "data[i]: " + repr(data[i])
+                print "data[" + repr(i) + "]: " + data[i].text_content()
                 i += 1
 #        neighborhood = fields[49].text_content().strip()
 #        apprData = {'parcelID': parcelID,
